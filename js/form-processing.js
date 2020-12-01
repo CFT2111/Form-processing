16a4ba47216ad375dfae02a34762f2250512a970
M Mantle <mmantlehud@googlemail.com>
2020-12-01 12:03:16 +0000

    Added full solutions

--- js/form-processing.js
+++ js/form-processing.js
@@ -8,17 +8,37 @@ const countries=[
 
 
 /*
-1) 
-a) Write a some JavaScript code that uses document.getElementById to grab hold of the button in the HTML page. Use a console.log message to check this has worked. 
+1)
+a) Write a some JavaScript code that uses document.getElementById to grab hold of the button in the HTML page. Use a console.log message to check this has worked.
 b) Add a click event listener to the button. Again use a simple console.log message to check this has worked.
 */
 
-
+/*
+const btn  = document.getElementById("formBtn");
+console.log(btn);
+btn.addEventListener("click",function(){
+  console.log("The event handling works");
+})
+*/
 
 /*
-2) Next try and do some form processing. Start with the text box. 
-a) Can you get whatever the user has typed into the text box to appear in the console. 
+2) Next try and do some form processing. Start with the text box.
+a) Can you get whatever the user has typed into the text box to appear in the console.
 b) Can you also display the user's choice of continent in the console.
+*/
+/*
+function processTheForm(){
+  const userName = document.getElementById("username").value;
+  console.log(userName);
+  const continent = document.getElementById("continent").value;
+  console.log(continent);
+}
+
+
+const btn  = document.getElementById("formBtn");
+btn.addEventListener("click",function(){
+  processTheForm();
+})
 */
 
 /*
@@ -26,11 +46,33 @@ b) Can you also display the user's choice of continent in the console.
 */
 
 /*
+function processTheForm(){
+  const userName = document.getElementById("username").value;
+  const continent = document.getElementById("continent").value;
+  const matchingCountries = countries.filter(function(country){
+    if(country.continent == continent){
+      return true;
+    }
+    return false;
+  })
+  console.log(`${userName}, here is a list of countries:`)
+  matchingCountries.forEach(function(country){
+    console.log(country.name);
+  })
+}
+
+const btn  = document.getElementById("formBtn");
+btn.addEventListener("click",function(){
+  processTheForm()
+})
+*/
+
+
+/*
 4. Can you add code that listens for a 'change' event on the select menu. Whenever the user selects a different continent the list of matching countries in the console should change.
 */
 
 /*
-//answers to questions 1-4
 function processTheForm(){
   const userName = document.getElementById("username").value;
   const continent = document.getElementById("continent").value;
@@ -61,9 +103,9 @@ selectMenu.addEventListener("change",function(){
 5. If you have got the above to work, think how you have organised and structured your code. It would probably be a good idea to have something like the following (main would be called from the event listener function). Re-factor yuor code to use a number of different functions.
 */
 
-
+/*
 function getUserName(){
-    const userName = document.getElementById("username").value;
+  const userName = document.getElementById("username").value;
   return userName;
 }
 function getContinent(){
@@ -88,7 +130,7 @@ function displayResults(userName, matchingCountries){
   }else{
     console.log(`${userName}, no countries match`)
   }
-    
+
 }
 function main(){
     const userName = getUserName();
@@ -106,3 +148,63 @@ const selectMenu  = document.getElementById("continent");
 selectMenu.addEventListener("change",function(){
   main()
 })
+
+*/
+
+/*
+6. Now think about how you can use a window.load event to trigger the set-up tasks for your application. Typically the load event would execute code that uses the DOM to select elements from the page and attach events to these elements.
+*/
+
+/*
+function getUserName(){
+  const userName = document.getElementById("username").value;
+  return userName;
+}
+
+function getContinent(){
+  const continent = document.getElementById("continent").value;
+  return continent;
+}
+function filterCountries(continent){
+  const matchingCountries = countries.filter(function(country){
+    if(country.continent == continent){
+     return true;
+    }
+    return false;
+  })
+  return matchingCountries
+}
+function displayResults(userName, matchingCountries){
+  if(matchingCountries.length>0){
+      console.log(`${userName}, here is a list of countries:`)
+      matchingCountries.forEach(function(country){
+        console.log(country.name);
+      })
+  }else{
+    console.log(`${userName}, no countries match`)
+  }
+
+}
+function main(){
+    const userName = getUserName();
+    const continent = getContinent();
+    const matchingCountries = filterCountries(continent);
+    displayResults(userName, matchingCountries);
+}
+
+function init(){
+  const btn  = document.getElementById("formBtn");
+  btn.addEventListener("click",function(){
+    main()
+  })
+  const selectMenu  = document.getElementById("continent");
+  selectMenu.addEventListener("change",function(){
+    main()
+  })
+}
+
+
+window.addEventListener("load",function(){
+  init();
+})
+*/
