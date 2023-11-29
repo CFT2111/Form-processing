ba925220578af8cf915834bb0e1c24891198262e
mmantle-hud <m.e.mantle@hud.ac.uk>
2023-11-29 15:34:41 +0000

    Update multiple-checkboxes.js

--- examples/multiple-checkboxes/multiple-checkboxes.js
+++ examples/multiple-checkboxes/multiple-checkboxes.js
@@ -1,8 +1,8 @@
 
 
 function processForm(){
-	const chkBoxes = document.querySelectorAll('input[type="checkbox"]:checked'); //select all the checkboxes that have been checked
- 	const selectedItems = []; //declare an empty array
+  const chkBoxes = document.querySelectorAll('input[type="checkbox"]:checked'); //select all the checkboxes that have been checked
+  const selectedItems = []; //declare an empty array
   chkBoxes.forEach(function(chkBox){ //loop over the selected checkboxes
     selectedItems.push(chkBox.value); //add the checkbox value to the selectedItems array
   })
@@ -11,7 +11,7 @@ function processForm(){
 
 
 
-const btn =  document.getElementById("myButton");
+const btn =  document.querySelector("#myButton");
 
 btn.addEventListener("click",function(){
 	processForm();
